b0bace99ccaf955115ea3deb962c090f78519fbc
Alex <tarkovskiy.alexandr@protonmail.com>
2023-06-05 15:23:40 +0300

    added tests

--- src/utils/compute-pool-address-zksync.test.ts
+++ src/utils/compute-pool-address-zksync.test.ts
@@ -1,27 +1,27 @@
-import { FeeAmount } from '../constants/types';
+import { FeeAmount } from '../constants';
 import { computePoolAddressZkSync } from './compute-pool-address-zksync';
 import { zkSyncTokens } from '../tokens';
 
 describe('#computePoolAddressZkSync', () => {
     it('should correctly compute the pool address', () => {
         const result = computePoolAddressZkSync({
-          fee: FeeAmount.LOW,
-          tokenA: zkSyncTokens.weth,
-          tokenB: zkSyncTokens.usdc,
+            fee: FeeAmount.LOW,
+            tokenA: zkSyncTokens.weth,
+            tokenB: zkSyncTokens.usdc,
         });
         expect(result).toEqual('0x668038485eABfAaeDc226AdC1F9cC4348a872711');
     });
 
     it('should correctly compute the pool address', () => {
         const resultA = computePoolAddressZkSync({
-          fee: FeeAmount.LOW,
-          tokenA: zkSyncTokens.weth,
-          tokenB: zkSyncTokens.usdc,
+            fee: FeeAmount.LOW,
+            tokenA: zkSyncTokens.weth,
+            tokenB: zkSyncTokens.usdc,
         });
         const resultB = computePoolAddressZkSync({
-          fee: FeeAmount.LOW,
-          tokenA: zkSyncTokens.usdc,
-          tokenB: zkSyncTokens.weth,
+            fee: FeeAmount.LOW,
+            tokenA: zkSyncTokens.usdc,
+            tokenB: zkSyncTokens.weth,
         });
         expect(resultA).toEqual(resultB);
     });
