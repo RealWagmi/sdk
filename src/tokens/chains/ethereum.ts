be64f270dac3537e1cdb8203db39dfc1862e1669
Alex <tarkovskiy.alexandr@protonmail.com>
2023-10-04 10:42:53 +0300

    update tokens

--- src/tokens/chains/ethereum.ts
+++ src/tokens/chains/ethereum.ts
@@ -4,4 +4,8 @@ import { ChainId } from '../../constants/chains';
 export const ethereumTokens = {
     weth: new Token(ChainId.ETHEREUM, '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', 18, 'WETH', 'Wrapped Ether'),
     wagmi: new Token(ChainId.ETHEREUM, '0x92CC36D66e9d739D50673d1f27929a371FB83a67', 18, 'WAGMI', 'Wagmi'),
+    usdc: new Token(ChainId.ETHEREUM, '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48', 6, 'USDC', 'USD Coin'),
+    usdt: new Token(ChainId.ETHEREUM, '0xdAC17F958D2ee523a2206206994597C13D831ec7', 6, 'USDT', 'Tether USD'),
+    wbtc: new Token(ChainId.ETHEREUM, '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599', 8, 'WBTC', 'Wrapped BTC'),
+    dai: new Token(ChainId.ETHEREUM, '0x6B175474E89094C44Da98b954EedeAC495271d0F', 18, 'DAI', 'Dai Stablecoin'),
 };
--- src/tokens/chains/kava.ts
+++ src/tokens/chains/kava.ts
@@ -4,4 +4,6 @@ import { ChainId } from '../../constants/chains';
 export const kavaTokens = {
     wkava: new Token(ChainId.KAVA, '0xc86c7C0eFbd6A49B35E8714C5f59D99De09A225b', 18, 'WKAVA', 'Wrapped Kava'),
     wagmi: new Token(ChainId.KAVA, '0xaf20f5f19698f1D19351028cd7103B63D30DE7d7', 18, 'WAGMI', 'Wagmi'),
+    usdc: new Token(ChainId.KAVA, '0xfa9343c3897324496a05fc75abed6bac29f8a40f', 6, 'USDC', 'USD Coin'),
+    usdt: new Token(ChainId.KAVA, '0x919c1c267bc06a7039e03fcc2ef738525769109c', 6, 'USDt', 'TetherUSDt')
 };
